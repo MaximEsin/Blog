7bd2b44fd816dbc0cae71fe59d8afc31643e8094
Maxim Esin <maximesin21@gmail.com>
2022-11-24 16:00:13 +0700

    Post item & post list components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
+import React from "react";
+import PostList from "./components/PostList";
+import "./styles/App.css";
+import { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState("TEXT INSIDE INPUT");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "HTML", body: "Description" },
+    { id: 3, title: "CSS", body: "Description" },
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python", body: "Description" },
+    { id: 2, title: "C#", body: "Description" },
+    { id: 3, title: "Java", body: "Description" },
+  ]);
 
   return (
     <div className="App">
-      <ClassCounter />
+      <PostList posts={posts} title={"List of front-end posts"} />
+      <PostList posts={posts2} title={"List of back-end posts"} />
     </div>
   );
 }
